8be5adb99a77ef5db129bedb6f585ac71da3af71
Aditya Diwakar <aditya@diwakar.io>
2022-10-29 11:26:11 -0400

    add more layout and console with working interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,66 @@
 #include <ncurses.h>
-using namespace std;
+#include <string>
 
 int main(int argc, char** argv) 
 {
   initscr();
+
   cbreak();
   noecho();
 
-  WINDOW *win = newwin((LINES - 1) / 2, (COLS - 1) / 3, 1, 1);
-  
+  WINDOW *status_win = newwin(3, (COLS - 1) / 3, 0, 0);
+  box(status_win, 0, 0);
+
+  WINDOW *quotes = newwin((LINES) / 2, (COLS) / 3, 3, 0);
+  box(quotes, 0, 0);
+
+  WINDOW *chart_win = newwin(2 * (LINES) / 3, 2 * (COLS) / 3, 0, (COLS) / 3);
+  box(chart_win, 0, 0);
+
+  WINDOW *console = newwin((LINES) / 3, 2 * (COLS) / 3, 2 * (LINES) / 3, (COLS) / 3);
+  box(console, 0, 0);
+
+  mvwprintw(quotes, 0, 1, "Quotes - Watchlist");
+  mvwprintw(status_win, 0, 1, "Status");
+  mvwprintw(chart_win, 0, 1, "Charts");
+  mvwprintw(console, 0, 1, "Console");
+
+  mvwprintw(status_win, 1, 1, "Establishing connection...");
+  refresh();
+
+  wmove(console, 1, 4);
+  refresh();
+  wrefresh(console);
+
+  std::string cons_line ("$> ");
+  cons_line.resize(2 * COLS / 3 - 2, ' ');
+  int cons_line_length = 3;
+  mvwprintw(console, 1, 1, "%s", cons_line.c_str());
+  wmove(console, 1, 1 + cons_line_length);
+
   for (;;) {
+    wrefresh(quotes);
+    wrefresh(chart_win);
+    wrefresh(status_win);
+    wrefresh(console);
+
+    int c = getch();
+    if (c == 'q') break;
+    if (c == KEY_BACKSPACE || c == 127) {
+      if (cons_line_length > 3) {
+        cons_line[cons_line_length - 1] = ' ';
+        cons_line_length--;
+      }
+    }
+    else {
+      cons_line[cons_line_length] = c;
+      cons_line_length++;
+    }
+
+    mvwprintw(console, 1, 1, "%s", cons_line.c_str());
+    wmove(console, 1, 1 + cons_line_length);
+
     refresh();
-    box(win, 0, 0);
-    mvwprintw(win, 0, 1, "Quotes");
-
-    wrefresh(win);
-
-    if (getch() == 'q') break;
   }
 
   endwin();
